55021dc557943619a48200a80604d22f168609d7
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-17 10:14:48 -0600

    tipado de variables con valor primitivos

--- Hello-world.ts
+++ Hello-world.ts
@@ -1,1 +1,11 @@
 console.log('hola mundo');
+
+// Tipado de objeto
+function printcoord(pt: { x: number, y: number}) {
+    console.log('rl vslor de las coordenas x es: ' +pt.x)
+    console.log('rl vslor de las coordenas y es: ' +pt.y)
+}
+
+printcoord({x: 2, y: 10})
+
+
---
+++ src/sesion-2/tipado-pato.ts
@@ -0,0 +1,4 @@
+//tipado pato
+
+let nameIdobject = {name: 'hugo', id: 1, print() {} }
+//nameIdobject = {id: 1, name: 'hugo' }
