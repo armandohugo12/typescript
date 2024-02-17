86f57ab0b8e7d426ba9f2f3f8498d66653bd827d
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-17 10:35:39 -0600

    se agrego un comentario sobre lo que se iso

--- src/sesion-2/let.ts
+++ src/sesion-2/let.ts
@@ -26,3 +26,9 @@ console.log(`index: ${index}`)*/
 
 // el resultado es index = 2
 // el resultado es index = 0
+
+/* si trabajamos con ts en crudo, posiblemente coincidamos con nombre de varibales reservadas por TS
+ * pera evitar esto podemos agregar la siguient configuración
+ * "moduleDetection": "force"
+ * o agregando export {} al inicio del archivo
+ * o simplemente cambiando el nombre de la variable*/
