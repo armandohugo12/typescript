3629740ee8c9bd10f530362f81631b2cb5ff979d
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-17 12:22:39 -0600

    Expreciones condicionales

---
+++ src/sesion-2/condicional.ts
@@ -0,0 +1,8 @@
+
+//es lo mismos que un If pero con menor codigo
+function mayorDeEdad(edad:number){
+    const message : string =edad > 17 ? 'es mayor de edad ' : 'es menor de edad'
+    console.log(message)
+}
+mayorDeEdad(10)
+mayorDeEdad(18)
