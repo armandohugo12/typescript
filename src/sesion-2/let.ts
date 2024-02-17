8d94ecb918ff42e0ba089d96edd32c087f8bf26a
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-17 10:33:27 -0600

    Explicacion de las varibles  VAR y LET

---
+++ src/sesion-2/let.ts
@@ -0,0 +1,28 @@
+
+// EL var es para que agarre las varibles globlar
+
+/*var index: number = 0 
+
+if (index == 0) {
+    var index: number = 2
+    console.log(`index: ${index}`)
+}
+
+console.log(`index: ${index}`)*/
+
+// el resultado es index = 2
+// el resultado es index = 2
+
+// El let es para variables locales 
+
+/*let index: number = 0 
+
+if (index == 0) {
+    let index: number = 2
+    console.log(`index: ${index}`)
+}
+
+console.log(`index: ${index}`)*/
+
+// el resultado es index = 2
+// el resultado es index = 0
