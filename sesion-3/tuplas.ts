cb43d0736b1ad415ee4ece4c41da9e66bdef4152
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-24 10:16:29 -0600

    Destructuracion

--- sesion-3/tuplas.ts
+++ sesion-3/tuplas.ts
@@ -9,4 +9,31 @@ tuple1 = ['test', true]
 //ejemplo erroneo
 //tuple1 = [false, 'test']
 
-console.log(tuple1)
+
+console.log(tuple1[0])
+console.log(tuple1[1])
+
+/**
+ * la destructuración nos ahorra lineas de código 
+ * evitando declarar en lineas separadas cada variable
+ * que corresponda al indice o valor de un arreglo u objeto
+ */
+let [a, b] = tuple1
+
+console.log(a, b)
+
+
+let complexObj = {
+    aNum: 1,
+    bString: 'test',
+    cBool: true
+}
+
+
+/**
+ * la destructuración no obliga a que se declaren todas las variebles de cada propiedad
+ * es decir podemos omitir valores de un objeto o arreglo
+ */
+let {aNum, bString, cBool} = complexObj
+
+console.log(aNum, bString, cBool)
