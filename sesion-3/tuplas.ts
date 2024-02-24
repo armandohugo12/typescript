b29018dfa9eb0d79e6167ab81b30e5b5f0968a4b
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-24 10:00:07 -0600

    Tuplas - Tipado de arreglo

---
+++ sesion-3/tuplas.ts
@@ -0,0 +1,12 @@
+let tuple1:[string, boolean]
+tuple1 = ['test', true]
+
+/**
+ * Ts nos ayuda a tipar a tipar arreglos y estos solo podran aceptar
+ * los valores que le hayyamos indicado
+ * 
+ */
+//ejemplo erroneo
+//tuple1 = [false, 'test']
+
+console.log(tuple1)
