b0b7347637b46ac4bec081e4f7969e9d6986bf65
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-02-27 18:24:16 -0600

    Interfaces - tipado estricto de objetos

---
+++ sesion-3/interface.ts
@@ -0,0 +1,30 @@
+interface IdName {
+    id: number;
+    name: string;
+    print: (text: string, age: number) => void
+}
+
+let idObj: IdName = {
+    id: 1,
+    name: 'Juan',
+    print: (text, age) => console.log(text, age)
+}
+
+idObj.print('hola', 2)
+
+
+interface Optional {
+    id: number;
+    name?: string;
+}
+
+let optional: Optional = {
+    id: 2
+}
+
+interface WeakType {
+    id?: number;
+    name?: string;
+}
+
+let weak: WeakType = {}
