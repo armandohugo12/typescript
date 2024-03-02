e66c3eb269f40d55f78f79b7f9df636226e98933
armandohugo12 <hugo.illan3023@alumnos.udg.mx>
2024-03-02 11:24:12 -0600

    encapsulamiento

--- src/sesion-4/clases.ts
+++ src/sesion-4/clases.ts
@@ -1,12 +1,28 @@
-class SimpleClass {
-    id: number | undefined 
-    print() {
-        console.log('se llamo el metodo pront de la clase simpleClass')
+class SimpleClasss {
+    private id: number
+    readonly name: string
+
+    constructor(name: string) {
+        this.id = 1
+        this.name = name
     }
+
+    // public no es necesario señalarlo ya que es por defecto
+    public print() {
+        console.log('simpleClass.id = ' + this.id)
+    }
+
+    /**
+     * las propiedades tipo readonly solo se puede asignar 
+     * valores dentro de constructor de la clase
+     */
+    /* setName(name: string) {
+        this.name = name
+    } */
 }
 
-let simpleClass = new SimpleClass()
-let simpleClass2 = new SimpleClass()
+let SimpleClass = new SimpleClasss('juan')
 
-simpleClass.print()
-simpleClass2.print()
+// simpleClass.name = ''
+
+SimpleClass.print()
